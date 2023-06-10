309dcfe5150b2962668fb6385a9471a0bd365d40
qwertytr1 <makhnach20040208@gmail.com>
2023-06-10 12:28:03 +0300

    feat:change data

--- src/components/UI Elements/sorted element/manufacture/manufacture.tsx
+++ src/components/UI Elements/sorted element/manufacture/manufacture.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './manufacture.css';
+import { allManufacturers } from 'data/data';
 function Manufacture() {
   return (
     <aside className="main__filter__manufactures-block">
       <h2 className="categories manufacturers">Manufacturers</h2>
-      <div className="main__filter__manufacturers"></div>
+      <div className="main__filter__manufacturers">
+        <div className="main__filter__categories __manufacturers">
+          {allManufacturers.map((name) => (
+            <label key={name} className="main__filter-categories-item">
+              <input type="checkbox" className="main__filter-categories-checkbox" />
+              {name}
+            </label>
+          ))}
+        </div>
+      </div>
     </aside>
   );
 }
